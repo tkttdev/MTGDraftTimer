f932185c6e82d92ae9190f0f0dc31a010c7630ea
tkttdev <takamiya.dev@gmail.com>
2017-05-23 05:00:25 +0900

    Modify UI Add se

--- Assets/Application/Scripts/MainController.cs
+++ Assets/Application/Scripts/MainController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainController : MonoBehaviour {
 
 	[SerializeField]private TimeManager timeManager;
 	[SerializeField]private GameObject startButton;
 	[SerializeField]private GameObject pauseButton;
+	[SerializeField]private GameObject resumeButton;
+	[SerializeField]private GameObject exitButton;
 
 	private enum TimerStatus : int {
 		WAIT = 0,
@@ -17,6 +20,7 @@ public class MainController : MonoBehaviour {
 	TimerStatus timerStatus = TimerStatus.WAIT;
 
 	public void StartButton() {
+		timeManager.Initialize();
 		timerStatus = TimerStatus.DRAFT;
 		startButton.SetActive(false);
 		pauseButton.SetActive(true);
@@ -24,14 +28,26 @@ public class MainController : MonoBehaviour {
 
 	public void PauseButton(){
 		timerStatus = TimerStatus.WAIT;
-		startButton.SetActive(true);
+		resumeButton.SetActive(true);
+		exitButton.SetActive(true);
 		pauseButton.SetActive(false);
+	}
+
+	public void ResumeButton() {
+		timerStatus = TimerStatus.DRAFT;
+		resumeButton.SetActive(false);
+		exitButton.SetActive(false);
+		pauseButton.SetActive(true);
+	}
+
+	public void ExitButton() {
+		SceneManager.LoadScene("Title");
 	}
 
 	private void Update(){
 		if(timerStatus == TimerStatus.DRAFT){
 			timeManager.TimerUpdate();
-			if((int)timeManager.draftStatus == 3){
+			if(timeManager.draftStatus == TimeManager.DraftStatus.FINISH){
 				timerStatus = TimerStatus.FINISH;
 			}
 		}
--- Assets/Application/Scripts/TimeManager.cs
+++ Assets/Application/Scripts/TimeManager.cs
@@ -5,33 +5,38 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour {
 	[SerializeField]private Text timerText;
-	[SerializeField]private Text StatusText;
-	private readonly float[] specifiedDraftTime = new float[15]{40f, 40f, 35f, 30f, 25f, 25f, 20f, 20f, 15f, 10f, 10f, 5f, 5f, 5f, 1f};
+	[SerializeField]private Text statusText;
+	[SerializeField]private Text packCountText;
+	private readonly float[] specifiedDraftTime = new float[15]{40f, 40f, 35f, 30f, 25f, 25f, 20f, 20f, 15f, 10f, 10f, 5f, 5f, 5f, 3f};
 	private readonly float[] specifiedPackIntervalTime = new float[3]{60f, 90f, 120f};
-	private const float exchangeInterval = 3.0f;
+	private const float exchangeIntervalTime = 3.0f;
+	private const float openPackTime = 8.0f;
+	private const float constructDeckTime = 180f;
 	private float time = 0.0f;
 	private int countsOfPack = 0;
 	private int countsOfPick = 0;
+	[SerializeField]private AudioSource audioSource;
+	[SerializeField]private AudioClip audioClip;
 
 	public enum DraftStatus : int {
 		PICK = 0,
 		EXCHANGE = 1,
 		CONFIRM_PICK = 2,
-		FINISH = 3,
+		OPEN_PACK = 3,
+		CONSTRUCT = 4,
+		FINISH = 5,
 	}
 
 	public DraftStatus draftStatus = DraftStatus.PICK;
 
-	private void Awake(){
+	public void Initialize() {
 		draftStatus = DraftStatus.PICK;
 		time = specifiedDraftTime[countsOfPick++];
-		countsOfPick = 14;
-		countsOfPack = 2;
-		SetNextDraft();
+		packCountText.text = "1PACK";
 	}
 
 	public void TimerUpdate(){
-		StatusText.text = draftStatus.ToString();
+		statusText.text = draftStatus.ToString();
 		time = time - Time.deltaTime > 0f ? time - Time.deltaTime : 0f;
 		timerText.text = string.Format("{0:f0}", time);
 		if(Mathf.Abs(time) < Mathf.Epsilon){
@@ -43,22 +48,31 @@ public class TimeManager : MonoBehaviour {
 		if(draftStatus == DraftStatus.PICK){
 			if(countsOfPick != 15){
 				draftStatus = DraftStatus.EXCHANGE;
-				time = exchangeInterval;
+				time = exchangeIntervalTime;
 			} else {
 				draftStatus = DraftStatus.CONFIRM_PICK;
 				countsOfPick = 0;
-				time = specifiedPackIntervalTime[countsOfPack++];
 			}
 		} else if(draftStatus == DraftStatus.EXCHANGE){
 			draftStatus = DraftStatus.PICK;
 			time = specifiedDraftTime[countsOfPick++];
 		} else if(draftStatus == DraftStatus.CONFIRM_PICK){
-			if(countsOfPack != 3){
-				draftStatus = DraftStatus.PICK;
-				time = specifiedDraftTime[countsOfPack++];
+			if(countsOfPack != 2){
+				packCountText.text = string.Format("{0}PACK", countsOfPack + 1);
+				draftStatus = DraftStatus.OPEN_PACK;
+				time = openPackTime;
 			} else {
-				draftStatus = DraftStatus.FINISH;
+				draftStatus = DraftStatus.CONSTRUCT;
+				time = constructDeckTime;
 			}
+		} else if(draftStatus == DraftStatus.OPEN_PACK){
+			draftStatus = DraftStatus.PICK;
+			time = specifiedDraftTime[countsOfPack++];
+		} else if(draftStatus ==  DraftStatus.CONSTRUCT){
+			draftStatus = DraftStatus.FINISH;
+			statusText.text = "FINISH";
 		}
+		audioSource.clip = audioClip;
+		audioSource.Play();
 	}
 }
