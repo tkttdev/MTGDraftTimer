5e31d74548408fd8898fabb1f285b86174fe067e
tatsuhito <takamiya.dev@gmail.com>
2017-05-23 05:26:15 +0900

    Remove extra fonts Fixed minor bug

--- Assets/Application/Scripts/TitleController.cs
+++ Assets/Application/Scripts/TitleController.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour {
-
-	void Update () {
-		if(Input.GetMouseButtonDown(0)){
-			SceneManager.LoadScene("Main");
-		}
+	public void MenuButton(){
+		SceneManager.LoadScene ("Main");
 	}
 }
